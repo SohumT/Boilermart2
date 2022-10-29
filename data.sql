c7526a368d9a6481472b39b36a79f5ba9767964c
SohumT <70512473+SohumT@users.noreply.github.com>
2022-10-29 06:19:21 -0400

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,22 @@
+-- Users
+
+Insert into Users values (1, "test1", "123456", 1, 1, "410 Purdue Mall", 47906);
+
+-- Companies 
+
+Insert into company values (1, "Walmart Inc", 75.6);
+Insert into company values (2, "Kroger Co", 20.5);
+
+-- Stores 
+
+Insert into stores values(1, "Walmart Supercenter", 1, "South Creasy Lane", 47905);
+Insert into stores values(2, "Sams Club South St", 1, "South St", 47905);
+Insert into stores values(3, "Pay Less ", 2, "Sagamore Pkwy", 47906);
+
+-- Items 
+
+Insert into items values(1, 1, "banana", 1.95, 1.04, "food", 50);
+Insert into items values(2, 1, "apple", 0.95, 1.05, "food", 60);
+Insert into items values(3, 1, "Playstation 5", 500, 3.05, "electronics", 0);
+Insert into items values(4, 2, "croisants", 3.94, 1.25, "food", 6);
+Insert into items values(5, 3, "croisants", 3.94, 1.25, "food", 7);
---
+++ schema.sql
@@ -0,0 +1,46 @@
+
+CREATE TABLE stores(
+  store_id integer PRIMARY KEY,
+  store_name varchar(50),
+  company_id integer,
+  address varchar(50),
+  zipcode integer
+); 
+
+CREATE TABLE items( 
+  item_id integer,
+  store_id integer,
+  name varchar(50),
+  price integer,
+  weight decimal(4,1),
+  category varchar(50),
+  stock integer,
+  PRIMARY KEY (item_id)
+);
+
+CREATE TABLE discounts(
+  store_id integer,
+  item_id integer,
+  sale_name varchar(50),
+  percentage decimal(2,2),
+  PRIMARY KEY(store_id, item_id)
+); 
+
+CREATE TABLE company(
+  company_id integer,
+  name text,
+  sales integer,
+  PRIMARY KEY (company_id)
+); 
+
+CREATE TABLE Users(
+  User_id integer,
+  Username varchar(50),
+  Password text,
+  Last_Frequented_Store int, 
+  Last_Item int,
+  address text,
+  zipcode integer,
+  PRIMARY Key (User_id)
+);
+
