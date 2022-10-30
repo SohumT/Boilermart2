0cbf6bae1b0ead558907aad6dbc945f92690e31a
tkim2000 <106344190+tkim2000@users.noreply.github.com>
2022-10-30 14:44:28 -0400

    Add files via upload
    Search items by category

---
+++ categories.py
@@ -0,0 +1,92 @@
+#import streamlit as st
+from select import select
+import streamlit as st
+import mysql.connector as conn
+from streamlit_option_menu import option_menu
+from google.cloud import storage 
+import pandas as pd
+
+#import app_text1.py as utils
+
+#from python_mysql_dbconfig import read_db_config
+from PIL import Image
+
+
+def searchFunc(item):
+    if item:
+        print(item)
+
+    
+def main():
+
+    # image = Image.open('logo.png')
+    # st.image(image, width=300px)
+
+    
+      
+    # Title 
+    st.title("Boilermart")
+
+    # Drop Down Menu
+
+    cnx = conn.connect(user='root', password='12345678', host='104.198.25.233', 
+                              database='db1')
+    cursor = cnx.cursor()
+
+    query1 = ("select * from get_categories")
+    cursor.execute(query1)
+    temp = cursor.fetchall()
+    cursor.close()
+    category_list = []
+    category_list.append("<SELECT A CATEGORY>")
+    for i in temp:
+        category_list.append(i[0])
+
+    selected_cat = st.selectbox("search by category",options = category_list)
+    cursor = cnx.cursor()
+
+    loginQuery = "SELECT * from Users A WHERE A.username = ? and A.password = ?;"
+#    cursor.execute(loginQuery, (username, password))
+
+    if selected_cat is not "<SELECT A CATEGORY>":
+        print(selected_cat)
+
+        #get_items = ("CALL get_item_category("+selected_cat+");")
+        args = (selected_cat,)
+        
+        
+        cursor.callproc("get_item_category", args)
+
+        cursor.close()
+
+        cursor = cnx.cursor()
+
+        query1 = ("select * from result")
+        cursor.execute(query1)
+        data = cursor.fetchall()
+        cursor.close()
+
+        print(type(temp[0]))
+        print(temp[0])
+
+        df = pd.DataFrame(data, columns =['item_id', 'store_id', 'name', 'price', 'weight', 'category', 'stock'])
+
+        print(df)
+        st.table(df)
+
+
+
+
+
+
+
+
+       # print(temp)
+        
+
+
+
+    
+
+if __name__ == "__main__":
+    main()
