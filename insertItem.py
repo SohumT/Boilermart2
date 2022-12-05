5929b890023a54aa09efcc6f1d562b0cdf8d8a99
SohumT <ssthadan@purdue.edu>
2022-12-05 18:07:06 -0500

    insertFeature Fixed

--- insertItem.py
+++ insertItem.py
@@ -13,19 +13,13 @@ def insertItem(itemName, store_id, price, weight, category, quantity) -> pd.Data
                               database=connDet.database)
     cursor = cnx.cursor()
 
-    args = (store_id, itemName, price, weight, category, quantity)
+    args = (itemName, store_id, price, weight, category, quantity)
 
     print(args)
 
-    #cursor.callproc("insertItem", args)
+    cursor.callproc("insertItem", args)
 
-    #cursor.close()
-
-    insertQuery = "INSERT INTO items "\
-               "(item_id, store_id, name, price, weight, category, stock)"\
-               "VALUES (NULL, %s, %s, %s, %s, %s, %s)"
-
-    cursor.execute(insertQuery, args)
+    cursor.close()
 
     cnx.commit()
 
@@ -36,7 +30,7 @@ def getAllCategories() -> list:
                               database=connDet.database)
     cursor = cnx.cursor()
 
-    query1 = ("select * from get_categories")
+    query1 = ("select name from category")
     cursor.execute(query1)
     temp = cursor.fetchall()
     cursor.close()
@@ -110,33 +104,9 @@ def insertPage():
 
     # Search Button
     if itemName and weight and quantity and price and category and store:
-        st.button('Add Item', on_click=insertItem, args=(itemName, all_stores[store], price, weight, category, quantity))
-        df = showItems()
-        st.dataframe(df)
-
-        cnx = conn.connect(user='root', password='12345678', host='104.198.25.233', 
-                              database='boilermart')
-    #deletion = ("delete from dis_result")
-    cursor = cnx.cursor()
-    #cursor.execute(deletion)
-
-    #get_discounts = ("INSERT INTO dis_result SELECT items.name, store.names, items.price * (1 - discounts.percentage) as discounted_price from discounts, items, stores WHERE items.item_id == discounts.item_id AND store.store_id == items.store_id ORDER BY discounts.percentage")
-    get_discounts = ("SELECT discounts.sale_name, items.name, stores.store_name, items.price, (items.price * (1 - discounts.percentage)) as discounted_price from discounts, items, stores WHERE items.item_id = discounts.item_id AND stores.store_id = items.store_id ORDER BY discounts.percentage")
-    cursor.execute(get_discounts)
-    #retrieval = ("SELECT * FROM dis_result")
-    #cursor.execute(retrieval)
-    discounts = cursor.fetchall()
-
-    result = []
-    for xs in discounts:
-        temp = []
-        for x in xs:
-            temp.append(str(x))
-
-        result.append(tuple(temp))
-    df = pd.DataFrame(result, columns = ["Sale", "On", "Available at", "Price", "Discounted Price"])
-    print(df)
-    st.table(df)
+        st.button('Add Item', on_click=insertItem, args=(itemName, store, price, weight, category, quantity))
+        #df = showItems()
+        #st.dataframe(df)
     # Drop Down Menu
     #categories = ['food','electronics']
     # selected_cat = st.selectbox("search by category",options = categories)
